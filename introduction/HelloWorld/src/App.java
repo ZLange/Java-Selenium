bd9f5461521e7329344147ec3c120292c0466477
zozol <z.ozola.ozolina@gmail.com>
2023-03-14 09:26:20 +0200

    second test

--- introduction/HelloWorld/src/App.java
+++ introduction/HelloWorld/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // this is my first java test
+        /*
+         * another way to comment in Java
+         * another test line here
+         *
+         */
+
         System.out.println("Hello, World!");
+        System.out.println("Second line!");
+
     }
 }
